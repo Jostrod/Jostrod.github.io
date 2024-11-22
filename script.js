37cfb5a903a3c8002f58fdb97047b48c0c016aa8
Jostrod <jostein.rodahl@hotmail.com>
2024-11-22 09:32:17 +0100

    Update script.js
    updated script

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
 
+function toggleMenu() {
+    
+    const sidebar = document.getElementById('sidebar');
+
+    // Legg til eller fjern klassen "open" for å åpne/lukke menyen
+    sidebar.classList.toggle('open');
+
+    // Juster marginen til hovedinnholdet når menyen er åpen
+    if (sidebar.classList.contains('open')) {
+        document.body.style.marginLeft = '250px'; 
+    } else {
+        document.body.style.marginLeft = '0'; 
+    }
+}
+
+// Legg til smooth scroll for meny
+document.querySelectorAll('.sidebar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); 
+
+       
         const targetId = this.getAttribute('href').substring(1);
-        document.getElementById(targetId).scrollIntoView({
+        const targetSection = document.getElementById(targetId);
+
+        
+        targetSection.scrollIntoView({
             behavior: 'smooth'
         });
     });
 });
+
+
